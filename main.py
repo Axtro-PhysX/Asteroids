11c8844a2f36acd4e58f6bb4d37168c7166d4136
Nisanth <70921921+Axtro-PhysX@users.noreply.github.com>
2025-01-02 19:47:08 -0500

    add delta time calc and frame rate limit

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     # Initialize pygame
     pygame.init()
 
+    # Set Delta Time to 60 frames per second to not stress CPU
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,6 +22,7 @@ def main():
                 return
         screen.fill(color=black)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
