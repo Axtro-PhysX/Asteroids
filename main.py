c32d20bc3fba9154974ddfb88141ab0fe1dab316
Nisanth <70921921+Axtro-PhysX@users.noreply.github.com>
2025-01-03 11:33:22 -0500

    add pygame groups, implement in game loop

--- main.py
+++ main.py
@@ -20,13 +20,18 @@ def main():
     # spawn the player ship before the game loop
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # instantiate groups before the game loop
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
